8e919a333997f3ee287423c2cd93a8f760de660e
Dave Webb <davedwebb8211@gmail.com>
2011-09-29 14:50:57 +0200

    libdmtx update to 0.7.4 (Data Matrix 2D barcode library)

--- Library/Formula/libdmtx.rb
+++ Library/Formula/libdmtx.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libdmtx < Formula
-  url 'http://downloads.sourceforge.net/project/libdmtx/libdmtx/0.7.2/libdmtx-0.7.2.tar.bz2'
+  url 'http://downloads.sourceforge.net/project/libdmtx/libdmtx/0.7.4/libdmtx-0.7.4.tar.bz2'
   homepage 'http://www.libdmtx.org'
-  md5 '0684cf3857591e777b57248d652444ae'
+  md5 'd3a4c0becd92895eb606dbdb78b023e2'
 
   depends_on 'pkg-config' => :build
   depends_on 'imagemagick'
